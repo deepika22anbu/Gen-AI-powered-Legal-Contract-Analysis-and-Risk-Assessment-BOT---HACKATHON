8d31bf04072919c88be4295cec426a0b10804d33
deepika22anbu <deepika221998@gmail.com>
2026-02-25 16:44:06 +0530

    Update ingestionLayer.py

--- ingestionLayer.py
+++ ingestionLayer.py
@@ -4,7 +4,14 @@ import datetime
 import spacy
 import re
 
-nlp = spacy.load("en_core_web_sm")
+
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_clauses(text):
     """
@@ -69,3 +76,4 @@ def build_storage_json(raw_text, filename):
         })
 
     return storage
+
